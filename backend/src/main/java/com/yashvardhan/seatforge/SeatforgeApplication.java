47f5e7bab305fd5206c5fc9ce356ee829aa7041d
yashvardhaaan <yashvardhanmahamuni@gmail.com>
2026-06-28 13:19:45 +0530

    Day 1: Initial Spring Boot setup

---
+++ backend/src/main/java/com/yashvardhan/seatforge/SeatforgeApplication.java
@@ -0,0 +1,13 @@
+package com.yashvardhan.seatforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatforgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yashvardhan/seatforge/SeatforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvardhan.seatforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
